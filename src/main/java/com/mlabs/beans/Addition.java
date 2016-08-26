4fefa574ea995b721bcca30c48649b5713adbece
Sid <tech.sidhanta@gmail.com>
2016-08-26 15:35:42 +0530

    New Statement added
    Signed-off-by: Sid <tech.sidhanta@gmail.com>

--- src/main/java/com/mlabs/beans/Addition.java
+++ src/main/java/com/mlabs/beans/Addition.java
@@ -5,5 +5,6 @@ public class Addition {
 	public static void doAddition(int num1, int num2) {
 		int result = num1 + num2;
 		System.out.println("The Addition is :" + " " + result);
+		System.out.println("New Statement");
 	}
 }
